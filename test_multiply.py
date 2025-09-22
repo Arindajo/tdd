56c2f61ad5bf4a6fe1cfc200c8d02a2b4c540512
Arinda <144229704+Arindajo@users.noreply.github.com>
2025-09-22 15:03:22 +0300

    the test functions

--- test_multiply.py
+++ test_multiply.py
@@ -7,7 +7,10 @@ def test_second():
     assert multiply(2,2)==4  
 
 def test_third():
-    assert multiply(3,3) ==9    
+    assert multiply(3,3) ==9   
+
+def test_fourth():
+    assert multiply(4,4)==16
 
 def test_large_numbers():
     assert multiply(34,56)==34*56    
